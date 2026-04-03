e37266f1de69542714005ee6518e77232ab4cc96
pasqualeserra <pasqualeserra2007@gmail.com>
2026-04-03 11:00:39 +0200

    Dichiarazione e definizione della struttura nodo e della struttura contenitore della coda, entrambe nell file di implementazione

---
+++ queue_list.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "it.h"
+#include "queue_list.h"
+
+struct node {
+int item;
+struct node* next;
+}; 
+
+struct c_queue{
+struct node* tail;
+struct node* head;
+int size;
+};
