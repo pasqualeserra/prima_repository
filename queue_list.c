68ba02c1b0918234e8d97eba8116ee0ea3982835
pasqualeserra <pasqualeserra2007@gmail.com>
2026-04-03 22:08:33 +0200

    Definizione della funzione di creazione coda
    Implementazione della funzione per creare la coda, la coda creata sarà vuota,  lo spazio per i puntatori di testa e coda non sarà allocato.

--- queue_list.c
+++ queue_list.c
@@ -14,3 +14,22 @@ struct node* head;
 int size;
 };
 
+queue newQueue()
+{
+    queue q; //creating the queue variable
+    
+    q = (queue)malloc(sizeof(struct c_queue)); //allocating dinamic memory in the heap for the pointer variable
+    
+    if( q == NULL )// verifiying if the allocation gone wrong 
+    {
+        printf("Not enough memory to create the queue."); //warning the user
+    
+        return NULL; //returning NULL value
+    }
+
+    q->head = NULL; //initializing the queue's head pointer at NULL value
+    q->tail = NULL; //initializing the queue's tail pointer at NULL value
+    q->size = 0; //initializing the queue's size at 0
+    
+    return q; //returning the queue variable
+}
