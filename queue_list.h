c9524df697cd9aebdec68c3e42d1f41516653a63
pasqualeserra <pasqualeserra2007@gmail.com>
2026-04-03 11:50:59 +0200

    dichiarazione delle operazioni di base per la coda e descrizione veloce di queste

--- queue_list.h
+++ queue_list.h
@@ -1,1 +1,10 @@
-typedef struct c_queue* queue;
+typedef struct c_queue* queue; //definition of the queue type
+
+queue newQueue(); //creates a new empty queue and returns a pointer to it 
+
+int emptyQueue( queue q); //verifies if the queue is empty: returns 1 if the queue is empty otherwise if 0
+
+int enqueue(queue q, int e); //add the element e to the tail of the queue and returns 1 if the operation goes right otherwise 0
+
+int dequeue(queue q); //remove the element at the head of the queue , returns 1 if the operation goes right otherwise 0
+
