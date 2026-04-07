c5d79082e8febbebbf2c117f2393a935c7321962
pasqualeserra <pasqualeserra2007@gmail.com>
2026-04-07 10:44:23 +0200

    Creazione della funzione dequeue
    La funzione dequeue rimuove dalla coda l'elemento in testa e decrementa la grandezza della coda, inoltre libera lo spazio in memoria che serviva a contenere l'elemento rimosso

--- queue_list.c
+++ queue_list.c
@@ -63,3 +63,19 @@ int enqueue(queue q, int e)
     (q -> size)++; //increasing the size of the queue
     return 1; //returning 1
 }
+
+int dequeue( queue q)
+{
+    if(q -> size) //checking if the size of the queue is 0
+    {
+        printf("Impossible to remove an element in an empty queue."); //warning the user
+        return 0; //returning 0 to represent failure 
+    }
+
+    struct node* temp = q -> head; //declaring and initializing a node pointer variable to store the reference of the head's element
+    q->head = q->head->next; //updating the queue head's reference to the next element of the previous head 
+    free(temp); //freeing up the space in memory allocated to store the previous head element
+    (q -> size)--; //decreasing the queue's size
+
+    return 1; //returning 1 to represent success
+}
